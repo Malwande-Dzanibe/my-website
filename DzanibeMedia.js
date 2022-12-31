c95b8a2a517e90f494843ee31684a6e23cfdba89
Malwande-Dzanibe <malwandedza@gmail.com>
2022-12-31 02:14:05 +0200

    updated website

--- DzanibeMedia.js
+++ DzanibeMedia.js
@@ -28,43 +28,5 @@ document.addEventListener("scroll", function(){
     }
 })
 
-const projects = [
-    {
-        id : 01,
-        projectLink : "https://malwande-dzanibe.github.io/questions",
-        projectTittle : "Questions Project",
-        prjectImage : "./images/Screenshot (14).jpg"
-    },
-    {
-        id : 02,
-        projectLink : "https://malwande-dzanibe.github.io/cars",
-        projectTittle : "Favorite Cars Project",
-        prjectImage : "./images/01mercepexels-photo-120049.jpeg"
-    },
-    {
-        id : 03,
-        projectLink : "https://malwande-dzanibe.github.io/kota",
-        projectTittle : "Kota Menu Project",
-        prjectImage : "./FB_IMG_1663420490758.jpg"
-    },
-    {
-        id : 04,
-        projectLink : "https://malwande-dzanibe.github.io/clock",
-        projectTittle : "Digital Clock Project",
-        prjectImage : "./pexels-aphiwat-chuangchoem-359989.jpg"
-    },
-]
 
 
-
-window.addEventListener("DOMContentLoaded", function(){
-    showProjects()
-})
-
-function showProjects(){
-    const amaProject = projects.map(function(items){
-        return`<a class="p-title" href=${items.projectLink} target="_blank"><h4>${items.projectTittle}</h4></a>
-        <a class="p-link" href=${items.projectLink} target="_blank"><img class="p-image" src=${items.prjectImage}></a>`
-    }).join("")
-    projectsSection.innerHTML = amaProject
-}
